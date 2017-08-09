384b5772151426c79e87c96da08e746ef7063b1c
杨伊博 <527515025@qq.com>
2017-08-09 21:58:20 +0800

    modify class

---
+++ template/src/main/java/com/template/Hummer/Client.java
@@ -0,0 +1,27 @@
+package com.template.Hummer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * @author yangyibo
+ *
+ */
+public class Client {
+	public static void main(String[] args) throws IOException {
+		System.out.println("-------H1--------");
+		String type="0";
+//		String type = (new BufferedReader(new InputStreamReader(System.in))).toString();
+		HummerH1Model h1 = new HummerH1Model();
+		
+		if (type.equals("0")) {
+			h1.setAlarm(false);
+		}
+		h1.run();
+		System.out.println("\n-------H2--------");
+		HummerH2Model h2 = new HummerH2Model();
+		h2.run();
+	}
+}
