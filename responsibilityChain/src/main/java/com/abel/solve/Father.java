85513be272c3964c9e55992934a67cee8d9465cb
杨伊博 <527515025@qq.com>
2018-02-02 10:05:11 +0800

    add

---
+++ responsibilityChain/src/main/java/com/abel/solve/Father.java
@@ -0,0 +1,20 @@
+package com.abel.solve;
+
+import com.abel.problem.IWomen;
+
+/**
+ * Created by yangyibo on 2018/1/25.
+ */
+public class Father extends Handler {
+
+    public Father() {
+        super(Handler.FATHER_LEVEL_REQUEST);
+    }
+
+    // 父亲的答复
+    protected void response(IWomen women) {
+        System.out.println("女儿的请示是：");
+        System.out.println(women.getRequest());
+        System.out.println("父亲的回答： 同意");
+    }
+}
--- responsibilityChain/src/main/java/com/abel/solve/Handler.java
+++ responsibilityChain/src/main/java/com/abel/solve/Handler.java
@@ -1,7 +1,53 @@
 package com.abel.solve;
 
+import com.abel.problem.IWomen;
+
 /**
  * Created by yangyibo on 2018/1/24.
  */
 public abstract class Handler {
+
+    public final static int FATHER_LEVEL_REQUEST = 1;
+    public final static int HUSBAND_LEVEL_REQUEST = 2;
+    public final static int SON_LEVEL_REQUEST = 3;
+
+
+    //当前能处理的等级
+    private int level = 0;
+    //下一个责任人
+    private Handler nextHandler;
+
+    public Handler(int level){
+        this.level = level;
+    }
+
+    /**
+     * 一个女人要求逛街、等请求，需要处理
+     * 此处用到模版方法模式，在模版方法中判断请求的级别和当前能够处理的级别，如果相同则调用基本方法，作出反馈，如果不相等则传递到下一环节
+     * @param women
+     */
+    public final void HandleMessage(IWomen women){
+        if(women.getType() == this.level){
+            response(women);
+        }else{
+            if(null != this.nextHandler) {
+               // 有后续环节，将请求传递
+                this.nextHandler.HandleMessage(women);
+            }else{
+                // 无后续环节。
+                System.out.println("---没有后续环节，默认不同意---\n");
+            }
+        }
+    }
+
+    /**
+     * 找下一个环节的责任人
+     * @param _handler
+     */
+    public void setNext(Handler _handler){
+        this.nextHandler = _handler;
+    }
+
+    //有请示就要有回应
+    protected abstract void response(IWomen women);
 }
