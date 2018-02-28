db495b8f0c52f0ca5eabaf98e90dc46e486a8783
abel <527515025@qq.com>
2018-02-28 17:21:09 +0800

    add women

---
+++ responsibilityChain/src/main/java/com/abel/solve/Women.java
@@ -0,0 +1,29 @@
+package com.abel.solve;
+
+import com.abel.problem.IWomen;
+
+public class Women implements IWomen {
+
+    private int type = 0;
+    private String request;
+
+    public Women(int type, String request) {
+        this.type = type;
+        switch (type) {
+            case 1:
+                this.request = "女儿的请求是：" + request;
+            case 2:
+                this.request = "妻子的请求是：" + request;
+            case 3:
+                this.request = "母亲的请求是：" + request;
+        }
+    }
+
+    public int getType() {
+        return this.type;
+    }
+
+    public String getRequest() {
+        return this.request;
+    }
+}
